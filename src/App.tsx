252d0f5d78a7f34905141511a98c07ae01583f46
Moacir Petry <m.petry9@gmail.com>
2024-07-19 11:20:37 -0300

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <Navbar />
       </header>
+
+      <main className="flex-grow bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Technical Specifications
+          </h2>
+          <p className="mt-4 text-gray-500">
+            The walnut wood card tray is precision milled to perfectly fit a
+            stack of Focus cards. The powder coated steel divider separates
+            active cards from new ones, or can be used to archive important task
+            lists.
+          </p>
+        </div>
+      </main>
+
+      <footer className="bg-white rounded-lg shadow dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © {new Date().getFullYear()}{" "}
+            <a
+              href="https://moacirpetry.github.io/reactjs-on-gh-pages/"
+              className="hover:underline"
+            >
+              reactjs-on-gh-pages
+            </a>
+            . All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
